f666c79462817d856967dd76eb16d1690b9e7eca
Chad Walker <chad@montra.co>
2023-05-29 17:46:44 -0500

    add some basic types

---
+++ avalanche.d.ts
@@ -0,0 +1,59 @@
+/**
+ * (c) Chad Walker, Chris Kirmse
+ */
+
+type Metadata = {
+  video_encoding_name: string;
+  audio_encoding_name: string;
+
+  container_start_time: number;
+  container_duration: number;
+
+  video_start_time: number;
+  video_duration: number;
+  video_width: number;
+  video_height: number;
+  video_rate: number;
+};
+type ImageData = {
+  net_image_buffer: Buffer;
+  timestamp: number;
+  duration: number;
+};
+type ClipData = {
+  video_start_time: number;
+  video_duration: number;
+  count_video_packets: number;
+  count_key_frames: number;
+};
+type ProgressFunc = (step: number, total: number) => void;
+type VolumeData = {
+  mean_volume: number;
+  max_volume: number;
+};
+type ActionData = {
+  input: any[];
+  output: '<Running>' | 'exception' | boolean | VolumeData | ClipData | ImageData | Metadata;
+};
+export class LockedVideoReader {
+  constructor();
+  init(uri: string | ResourceIo = import('./resource_io')): Promise<boolean>;
+  drain(): void;
+  destroy(): Promise<null>;
+  verifyHasVideoStream(): Promise<boolean>;
+  verifyHasAudioStream(): Promise<boolean>;
+  getMetadata(): Promise<Metadata>;
+  getImageAtTimestamp(timestamp: number): Promise<ImageData>;
+  extractClipReencode(dest_uri: string, start_time: number, end_time: number, progress: ProgressFunc): Promise<ClipData>;
+  extractClipRemux(dest_uri: string, start_time: number, end_time: number, progress: ProgressFunc): Promise<ClipData>;
+  remux(dest_uri: string, progress: ProgressFunc): Promise<ClipData>;
+  getClipVolumeData(start_time: number, end_time: number, progress: ProgressFunc): Promise<VolumeData>;
+  getVolumeData(progress: ProgressFunc): Promise<VolumeData>;
+  getLatestAction(): ActionData;
+  getBlockedCount(): number;
+}
+
+export function createVideoReader(): LockedVideoReader;
+export function defaultLogAvalanche(level: 'info' | 'error' | 'warn' | 'debug', is_libav: boolean, str: string): void;
+export function setLogFunc(log_func: typeof defaultLogAvalanche): void;
+  
---
+++ data_source.d.ts
@@ -0,0 +1,20 @@
+/**
+ * (c) Chad Walker, Chris Kirmse
+ */
+
+export type DataSourceActivity = {
+  input_offset: number;
+  buffers_size: number;
+  is_in_data_request: boolean;
+  is_fetching: boolean;
+};
+
+export class DataSource {
+  constructor(url: string);
+  init(): Promise<void>;
+  getUrl(): string;
+  summarizeActivity(): DataSourceActivity;
+  getTotalSize(): number;
+  getLoadedSize(): number;
+  dataRequest(offset, count): Promise<Buffer[]>;
+};
